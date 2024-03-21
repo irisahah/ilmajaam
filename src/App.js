ee42c92a5ef1da68cbdce0e5f03e2f42ae2e6127
irisahah <irisharjo@gmail.com>
2024-03-21 18:55:56 +0200

    up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { loeAndmed } from './utils';
 import Asukohad from './Asukohad';
 import Detailid from './Detailid';
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
@@ -12,44 +12,47 @@ function App() {
       lat: 58.38588,
       long: 24.49711,
       andmed: null
-  },
+    },
     {
       nimetus: 'Tallinn',
       lat: 59.43696,
       long: 24.75353,
       andmed: null
-  },
+    },
     {
       nimetus: 'Tartu',
       lat: 58.38062,
       long: 26.72509,
       andmed: null
-  },
+    },
   ];
 
-  const loe =() => {
-    loeAndmed({lat: 58.38588, long: 24.49711})
-    loeAndmed({lat: 59.43696, long: 24.75353})
-  }
-
   const [aktiivne, setAktiivne] = useState(0)
+
+  const [ilmPraegu, setIlmPraegu] = useState(undefined)
 
   const muudaAktiivset = async (index) => {
     const koht = asukohad[index];
     setAktiivne(index)
-    const andmed = await loeAndmed({lat: koht.lat, long: koht.long})
+    const andmed = await loeAndmed({ lat: koht.lat, long: koht.long })
+    setIlmPraegu(andmed)
     console.log(andmed)
   }
+
+  useEffect(() => {
+    console.log('renderdus')
+    muudaAktiivset(0)
+  }, [])
 
   return (
     <div className="container">
       <h1>ILM</h1>
       <div className='row'>
         <div className='col-4'>
-        <Asukohad asukohad={asukohad} muudaAktiivset={muudaAktiivset} />
+          <Asukohad asukohad={asukohad} muudaAktiivset={muudaAktiivset} />
         </div>
         <div className='col-8'>
-        <Detailid koht={asukohad[aktiivne]}/>
+          <Detailid koht={asukohad[aktiivne]} ilmPraegu={ilmPraegu} />
         </div>
       </div>
     </div>
